3eb149d9f8a1f38d2061cfa318fd169251b7e827
ORCHORDS.COM <72497645+ORCHORDS@users.noreply.github.com>
2026-09-16 14:13:19 +0800

    chore(safety): remove unused report module

--- app/src/main/java/com/orchords/orchordsai/di/SafetyModule.kt
+++
@@ -1,8 +0,0 @@
-package com.orchords.orchordsai.di
-
-import com.orchords.orchordsai.data.safety.AiContentReportClient
-import org.koin.dsl.module
-
-val safetyModule = module {
-    single { AiContentReportClient(httpClient = get()) }
-}
